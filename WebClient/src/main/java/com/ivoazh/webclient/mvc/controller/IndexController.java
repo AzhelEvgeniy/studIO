3ced1f25fa9c92b4f8875d9b77ae4aa6ae047045
io <webworkk@mail.ru>
2018-02-17 14:01:51 +0300

    compited structure and init settings project

---
+++ WebClient/src/main/java/com/ivoazh/webclient/mvc/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.ivoazh.webclient.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = {"/", "home", "index"})
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("title", "Home");
+        mv.addObject("userClickHome", true);
+        return mv;
+    }
+
+}
